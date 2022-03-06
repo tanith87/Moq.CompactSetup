7703cd16fef11a4c60e28dc657a6095c5ecc2ec7
Christoph Meißner <christoph-meissner@gmx.net>
2022-03-06 11:09:01 +0100

    Class renaming to point to any reference modifier regardless it is ref or out.

---
+++ src/Moq.CompactSetup/MockExtensions.NewMock.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Moq.CompactSetup
+{
+    /// <summary>
+    /// The mock extensions class.
+    /// </summary>
+    public static partial class MockExtensions
+    {
+        /// <summary>
+        /// Gets the objects for the specified enumerable of mocks.
+        /// </summary>
+        /// <typeparam name="TElement"> The type of the element. </typeparam>
+        /// <param name="mockEnumerable"> The mock enumerable. </param>
+        /// <returns> The enumeration of objects. </returns>
+        /// <exception cref="ArgumentNullException"> When <paramref name="mockEnumerable"/> is <c>null</c>. </exception>
+        public static IEnumerable<TElement> Objects<TElement>(this IEnumerable<Mock<TElement>> mockEnumerable)
+            where TElement : class
+        {
+            if (mockEnumerable == null)
+            {
+                throw new ArgumentNullException(nameof(mockEnumerable));
+            }
+
+            return mockEnumerable.Select(x => x.Object);
+        }
+    }
+}
