feae202f74d9f545ec9e4951892c0224128d369c
Christoph Meißner <christoph-meissner@gmx.net>
2022-03-06 11:09:27 +0100

    This should have been just empty :-)

--- src/Moq.CompactSetup/MockExtensions.NewMock.cs
+++ src/Moq.CompactSetup/MockExtensions.NewMock.cs
@@ -9,22 +9,6 @@ namespace Moq.CompactSetup
     /// </summary>
     public static partial class MockExtensions
     {
-        /// <summary>
-        /// Gets the objects for the specified enumerable of mocks.
-        /// </summary>
-        /// <typeparam name="TElement"> The type of the element. </typeparam>
-        /// <param name="mockEnumerable"> The mock enumerable. </param>
-        /// <returns> The enumeration of objects. </returns>
-        /// <exception cref="ArgumentNullException"> When <paramref name="mockEnumerable"/> is <c>null</c>. </exception>
-        public static IEnumerable<TElement> Objects<TElement>(this IEnumerable<Mock<TElement>> mockEnumerable)
-            where TElement : class
-        {
-            if (mockEnumerable == null)
-            {
-                throw new ArgumentNullException(nameof(mockEnumerable));
-            }
-
-            return mockEnumerable.Select(x => x.Object);
-        }
+    
     }
 }
