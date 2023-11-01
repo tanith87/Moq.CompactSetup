1502a59a8fca28c76498e17e4e0abde69a35aa24
Christoph Meißner <christoph-meissner@gmx.net>
2023-11-01 13:48:35 +0100

    Added extension methods to create non-generic mocks from types.

--- src/Moq.CompactSetup/MockExtensions.General.cs
+++ src/Moq.CompactSetup/MockExtensions.General.cs
@@ -10,6 +10,41 @@ namespace Moq.CompactSetup
     public static partial class MockExtensions
     {
         /// <summary>
+        /// Creates a mock of the specified type.
+        /// </summary>
+        /// <param name="type"> The type to create a mock for. </param>
+        /// <param name="parameters"> The parameters to pass to the constructor if the type is a class. </param>
+        /// <returns> The created mock. </returns>
+        /// <exception cref="ArgumentNullException"> When <paramref name="type" /> is <see langword="null" />. </exception>
+        public static Mock CreateMock(this Type type, params object[] parameters)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            return (Mock)Activator.CreateInstance(typeof(Mock<>).MakeGenericType(type), parameters);
+        }
+
+        /// <summary>
+        /// Creates a mock of the specified type.
+        /// </summary>
+        /// <param name="type"> The type to create a mock for. </param>
+        /// <param name="mockBehavior"> The behavior of the mock. </param>
+        /// <param name="parameters"> The parameters to pass to the constructor if the type is a class. </param>
+        /// <returns> The created mock. </returns>
+        /// <exception cref="ArgumentNullException"> When <paramref name="type" /> is <see langword="null" />. </exception>
+        public static Mock CreateMock(this Type type, MockBehavior mockBehavior, params object[] parameters)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            return (Mock)Activator.CreateInstance(typeof(Mock<>).MakeGenericType(type), mockBehavior, parameters);
+        }
+
+        /// <summary>
         /// Gets the objects for the specified enumerable of mocks.
         /// </summary>
         /// <typeparam name="TElement"> The type of the element. </typeparam>
